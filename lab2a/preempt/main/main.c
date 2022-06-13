376701ede7d3273220f6b432349b6da008953848
goriummaximum <long.hothiengo@hcmut.edu.vn>
2022-06-13 21:23:38 +0700

    update log lab2a

--- lab2a/preempt/main/main.c
+++ lab2a/preempt/main/main.c
@@ -5,11 +5,6 @@
 #include "driver/gpio.h"
 
 /*
-code description:
-There are 3 tasks (priority): Idle (0) - Task2 (0) - Task1 (10)
-Task1 has highest priority and repeat for every 50 ticks
-Task2 and Idle have the same priority and run continuously.
-
 sdkconfig:
 - run only on first core
     + turn off FreeRTOS optimization
@@ -40,14 +35,14 @@ const char *LOG_TAG_VTASK3 = "VTASK3";
 const char *LOG_TAG_IDLE = "IDLE";
 
 void vApplicationIdleHook(void) {
-    ESP_LOGI(LOG_TAG_IDLE, "#%d, IdleTask running", xTaskGetTickCount());
+    ESP_LOGI(LOG_TAG_IDLE, "running");
 }
 
 
 void vTask1(void *pvParameters) {
     for (;;) {
-        ESP_LOGI(LOG_TAG_VTASK1, "#%d, vTask1 running", xTaskGetTickCount());
-        vTaskDelay(50);
+        ESP_LOGI(LOG_TAG_VTASK1, "running");
+        vTaskDelay(10);
     }
 
     vTaskDelete(NULL);
@@ -55,7 +50,7 @@ void vTask1(void *pvParameters) {
 
 void vTask2(void *pvParameters) {
     for (;;) {
-        ESP_LOGI(LOG_TAG_VTASK2, "#%d, vTask2 running", xTaskGetTickCount());
+        ESP_LOGI(LOG_TAG_VTASK2, "running");
     }
 
     vTaskDelete(NULL);
@@ -63,7 +58,7 @@ void vTask2(void *pvParameters) {
 
 void vTask3(void *pvParameters) {
     for (;;) {
-        ESP_LOGI(LOG_TAG_VTASK3, "#%d, vTask3 running", xTaskGetTickCount());
+        ESP_LOGI(LOG_TAG_VTASK3, "running");
     }
 
     vTaskDelete(NULL);
@@ -74,9 +69,9 @@ void app_main(void)
     //set priority for app_main to be highest among other tasks to avoid others task block app_main after initializaion
     vTaskPrioritySet(NULL, 15);
     
-    if (xTaskCreate(&vTask1, "vTask1", 2048, NULL, 10, NULL) == pdPASS) ESP_LOGI(LOG_TAG_MAIN, "vTask1 created successfully");
-    if (xTaskCreate(&vTask2, "vTask2", 2048, NULL, tskIDLE_PRIORITY, NULL) == pdPASS) ESP_LOGI(LOG_TAG_MAIN, "vTask2 created successfully");
-    //if (xTaskCreate(&vTask3, "vTask3", 2048, NULL, tskIDLE_PRIORITY, NULL) == pdPASS) ESP_LOGI(LOG_TAG_MAIN, "vTask3 created successfully");
+    if (xTaskCreatePinnedToCore(&vTask1, "vTask1", 2048, NULL, 10, NULL, 0) == pdPASS) ESP_LOGI(LOG_TAG_MAIN, "vTask1 created successfully");
+    if (xTaskCreatePinnedToCore(&vTask2, "vTask2", 2048, NULL, tskIDLE_PRIORITY, NULL, 0) == pdPASS) ESP_LOGI(LOG_TAG_MAIN, "vTask2 created successfully");
+    if (xTaskCreatePinnedToCore(&vTask3, "vTask3", 2048, NULL, tskIDLE_PRIORITY, NULL, 0) == pdPASS) ESP_LOGI(LOG_TAG_MAIN, "vTask3 created successfully");
     
     //set back to original priority for app_main task for not blocking other running tasks
     vTaskPrioritySet(NULL, 1);
